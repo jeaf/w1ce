8fd4b8a83bb7861cfbcfc13e1342752686b45f6c
Francois Jeannotte <francois.jeannotte@gmail.com>
2013-09-30 11:58:53 -0500

    Add minimal tests in test.c.

--- test.c
+++ test.c
@@ -1,7 +1,11 @@
 #include "w1ce.h"
 
+#include <stdio.h>
+
 int main()
 {
-    w1_isfile("abc");
+    printf("Should be true : %d\n", w1_isfile("w1ce.h"));
+    printf("Should be false: %d\n", w1_isfile("skjdfnskdw1ce.h"));
     return 0;
 }
+
