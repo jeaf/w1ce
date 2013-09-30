7cbcdd454ff20f33a8afd7278e5f9af09a4af89d
Francois Jeannotte <francois.jeannotte@gmail.com>
2013-09-30 10:50:22 -0500

    Implement isfile, isdir, abspath, and getch.

--- w1ce.c
+++ w1ce.c
@@ -1,7 +1,45 @@
 #include "w1ce.h"
 
+#ifdef _WIN32
+    #include <conio.h>
+    #include <Windows.h>
+#endif
+
 bool w1_isfile(const char* path)
 {
-    return false;
+    #ifdef _WIN32
+        DWORD a = GetFileAttributes(path);
+        return (a != INVALID_FILE_ATTRIBUTES) && !(a & FILE_ATTRIBUTE_DIRECTORY);
+    #else
+        #error w1_isfile is only implemented on win32
+    #endif
+}
+
+bool w1_isdir(const char* path)
+{
+    #ifdef _WIN32
+        DWORD a = GetFileAttributes(path);
+        return (a != INVALID_FILE_ATTRIBUTES) && (a & FILE_ATTRIBUTE_DIRECTORY);
+    #else
+        #error w1_isdir is only implemented on win32
+    #endif
+}
+
+unsigned int w1_abspath(const char* path, unsigned int buf_length, char* buf)
+{
+    #ifdef _WIN32
+        return GetFullPathName(path, buf_length, buf, 0);
+    #else
+        #error w1_abspath is only implemented on win32
+    #endif
+}
+
+int w1_getch()
+{
+    #ifdef _WIN32
+        return _getch();
+    #else
+        #error w1_abspath is only implemented on win32
+    #endif
 }
 
--- w1ce.h
+++ w1ce.h
@@ -1,4 +1,12 @@
+#ifndef W1CE_H
+#define W1CE_H
+
 #include <stdbool.h>
 
-bool w1_isfile(const char* path);
+bool         w1_isfile (const char* path);
+bool         w1_isdir  (const char* path);
+unsigned int w1_abspath(const char* path, unsigned int buf_length, char* buf);
+int          w1_getch();
+
+#endif
 
